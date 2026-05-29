82a5b3aace3b8862c744656ebcbc6fc110df6fdc
Raman-8688 <ramanms8688@gmail.com>
2026-05-29 09:50:32 +0530

    Restructure project into frontend and backend folders

--- bank-crud-app/src/main/java/com/learn/bank_crud_app/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.learn.bank_crud_app.repository;
-
-import com.learn.bank_crud_app.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-}
